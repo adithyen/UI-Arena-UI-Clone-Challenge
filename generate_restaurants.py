c2a98fdb3a4671a2303c51a8abfedc09e3c47ac1
adithyen <adityenh@gmail.com>
2026-08-08 19:23:33 +0530

    feat(zam-zam): Add authentic 11 dish menu for Zam Zam Dosa Hut

--- generate_restaurants.py
+++ generate_restaurants.py
@@ -5,7 +5,7 @@ import re
 restaurants = [
   { "id": 1, "name": "Pizza Hut", "rating": "4.4 (4.5K+ ratings)", "cost": "₹350 for two", "cuisines": "Pizzas", "time": "25–30 mins", "cover": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1600,h_640,c_fill/RX_THUMBNAIL/IMAGES/VENDOR/2026/6/15/4fc2475d-7187-41ee-b4b5-40c41fba3a94_448001.JPG" },
   { "id": 2, "name": "Hotel Chinnus", "rating": "4.6 (37K+ ratings)", "cost": "₹250 for two", "cuisines": "South Indian, Chinese", "time": "25–30 mins", "cover": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1600,h_640,c_fill/sfqa2q7svnybbjajui58" },
-  { "id": 3, "name": "Zam Zam Dosa Hut", "rating": "4.7 (10K+ ratings)", "cost": "₹300 for two", "cuisines": "South Indian, Biryani", "time": "25–30 mins", "cover": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1600,h_640,c_fill/RX_THUMBNAIL/IMAGES/VENDOR/2026/1/24/09c379a9-30bb-4bf5-bfa4-358055627685_323423.JPG" },
+  { "id": 3, "name": "Zam Zam Dosa Hut", "rating": "4.7 (30K+ ratings)", "cost": "₹349 for two", "cuisines": "South Indian, Biryani", "time": "40-50 mins", "cover": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1600,h_640,c_fill/bwqvepdiw71hg3w49you" },
   { "id": 4, "name": "Mother's Veg Plaza", "rating": "4.5 (8K+ ratings)", "cost": "₹200 for two", "cuisines": "South Indian, Kerala", "time": "30–40 mins", "cover": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1600,h_640,c_fill/RX_THUMBNAIL/IMAGES/VENDOR/2026/5/14/abcde_12345.JPG" },
   { "id": 5, "name": "Birdie Bite", "rating": "4.0 (2K+ ratings)", "cost": "₹400 for two", "cuisines": "North Indian, Fast Food", "time": "30–35 mins", "cover": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1600,h_640,c_fill/RX_THUMBNAIL/IMAGES/VENDOR/2026/2/1/birdie_123.jpg" },
   { "id": 6, "name": "The Cake Bros", "rating": "4.3 (1K+ ratings)", "cost": "₹300 for two", "cuisines": "Bakery, Desserts", "time": "25–30 mins", "cover": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1600,h_640,c_fill/RX_THUMBNAIL/IMAGES/VENDOR/2026/3/3/cakebros.jpg" },
@@ -78,33 +78,138 @@ chinnus_dishes = [
     }
 ]
 
+zam_zam_dishes = [
+    {
+        "name": "Mandi Rice",
+        "price": "149",
+        "rating": "2.8",
+        "rating_count": "(4)",
+        "desc": "1 Person Mandhi Rice",
+        "img": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/d325b710a96749763294c828ada9641e",
+        "veg": True
+    },
+    {
+        "name": "BOILED EGG",
+        "price": "25",
+        "rating": "4.7",
+        "rating_count": "(175)",
+        "desc": "",
+        "img": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/FOOD_CATALOG/IMAGES/CMS/2024/5/29/cb2d56ab-f204-4a15-a6ac-ae1bdc363a2d_e3e65e98-a639-4ff9-95f5-4ca1c39fba0f.png",
+        "veg": False
+    },
+    {
+        "name": "Cool Blue Crushers",
+        "price": "140",
+        "rating": "5.0",
+        "rating_count": "(133)",
+        "desc": "Serve with Bottle",
+        "img": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/xv3t9llqg5m3y3hyd803",
+        "veg": True
+    },
+    {
+        "name": "Mountain Crushers",
+        "price": "170",
+        "rating": "5.0",
+        "rating_count": "(49)",
+        "desc": "Serve with Bottle",
+        "img": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/drikdddfszvupqgszawx",
+        "veg": True
+    },
+    {
+        "name": "Plain Dosa",
+        "price": "102",
+        "rating": "4.7",
+        "rating_count": "(2.0K+)",
+        "desc": "",
+        "img": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/FOOD_CATALOG/IMAGES/CMS/2025/3/28/ef1e5e25-04b9-44c3-8440-b29f9b6ed7ab_f4a28e1d-fa98-4e67-a1ff-7292922aedd4.jpg",
+        "veg": True
+    },
+    {
+        "name": "Ghee Roast Dosa",
+        "price": "152",
+        "rating": "4.7",
+        "rating_count": "(2.5K+)",
+        "desc": "",
+        "img": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/FOOD_CATALOG/IMAGES/CMS/2025/3/28/ef1e5e25-04b9-44c3-8440-b29f9b6ed7ab_f4a28e1d-fa98-4e67-a1ff-7292922aedd4.jpg",
+        "veg": True
+    },
+    {
+        "name": "Masala Dosa",
+        "price": "170",
+        "rating": "4.7",
+        "rating_count": "(3.5K+)",
+        "desc": "[Serves 1]",
+        "img": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/72718c1cdb5db4de858344c9e82cbdc0",
+        "veg": True
+    },
+    {
+        "name": "Butter Dosa",
+        "price": "206",
+        "rating": "4.9",
+        "rating_count": "(169)",
+        "desc": "",
+        "img": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/FOOD_CATALOG/IMAGES/CMS/2025/3/28/ce9188f0-b2e2-4db9-b4b4-0c2799c6133e_d904fde6-806a-4aee-b0b2-a10071ffe714.jpg",
+        "veg": True
+    },
+    {
+        "name": "Egg Dosa",
+        "price": "163",
+        "rating": "4.5",
+        "rating_count": "(253)",
+        "desc": "",
+        "img": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/FOOD_CATALOG/IMAGES/CMS/2025/3/28/53501195-dfb2-4dec-b714-460c095b5e17_20022925-b866-4ab7-bb6e-f481cbea6b83.jpg",
+        "veg": False
+    },
+    {
+        "name": "Egg Masala Dosa",
+        "price": "215",
+        "rating": "4.7",
+        "rating_count": "(211)",
+        "desc": "",
+        "img": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/FOOD_CATALOG/IMAGES/CMS/2025/3/28/90e8eddf-6989-4b79-b505-a688ba30816f_a7208a57-6cae-4e31-8b20-45df24d5afc3.jpg",
+        "veg": False
+    },
+    {
+        "name": "Cheese Dosa",
+        "price": "198",
+        "rating": "4.6",
+        "rating_count": "(176)",
+        "desc": "",
+        "img": "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_300,h_300,c_fit/FOOD_CATALOG/IMAGES/CMS/2025/3/28/b9c9c936-8052-4575-8668-c609f1ddfd77_0c111668-b7f7-46a0-ae75-1a3a2462ea37.jpg",
+        "veg": True
+    }
+]
+
 def render_dish(dish, dish_id):
     veg_icon = 'vegVeg16' if dish['veg'] else 'nonvegNonVeg16'
-    color = '#116649' if float(dish['rating']) >= 4.0 else '#1BA672'
-    return f"""<div><div data-testid="normal-dish-item" class="sc-jiVjYv gRIQgR">
+    color = '#116649' if float(dish['rating']) >= 4.0 else '#E6A408'
+    veg_str = 'veg' if dish['veg'] else 'nonveg'
+    desc_html = f'<div class="sc-fUfliA iGdIQY"><div aria-hidden="true" class="sc-dlfnOL ipuZJn sc-jSUnEA jaouuY">{dish["desc"]}</div></div>' if dish['desc'] else ''
+    
+    return f"""<div data-testid="normal-dish-item" class="sc-jiVjYv gRIQgR" data-id="dish-{dish_id}" data-name="{dish['name'].lower()}" data-type="{veg_str}" data-bestseller="true">
   <div class="sc-goLLcu jEJsYT">
-    <p class="_1QbUq" tabindex="0">{dish['name']}. Costs: {dish['price']} rupees, Description: {dish['desc']} Swipe right to add item to cart.</p>
     <div>
       <div aria-hidden="true" class="sc-eWmBjE bSWzBq">
-        <svg aria-hidden="true" height="16" width="16" class="sc-eWVJoQ cxxFkr"><use xlink:href="/food/sprite-0RhsIk92.svg#{veg_icon}"></use></svg>
+        <svg viewBox="0 0 20 20" width="16" height="16" class="sc-eWVJoQ cxxFkr">
+          <use xlink:href="/food/sprite-0RhsIk92.svg#{veg_icon}"></use>
+        </svg>
       </div>
       <h3 aria-hidden="true" class="sc-dlfnOL hJCbXn sc-bxIdbF hEeFlr">{dish['name']}</h3>
       <div class="sc-kYpOdn gRmCvj">
         <div aria-hidden="true" class="sc-jSgsbC ewmHof">
-          <span class="sc-eCsseJ kiCBpN"><div class="sc-dlfnOL cWZrLo">{dish['price']}</div></span>
+          <span class="sc-eCsseJ kiCBpN"><div class="sc-dlfnOL cWZrLo">₹{dish['price']}</div></span>
         </div>
       </div>
       <div class="sc-jDWblu ciPLFu">
         <div class="sc-eeLFso bPRWxu">
-          <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg" role="img" fillColor="{color}"><rect width="14" height="14" fill="transparent"></rect><path d="M5.67163 3.99166C6.22068 2.34179 6.49521 1.51686 7 1.51686C7.50479 1.51686 7.77932 2.34179 8.32837 3.99166L8.65248 4.96556H9.60668C11.4122 4.96556 12.315 4.96556 12.4703 5.45302C12.6256 5.94049 11.8893 6.4628 10.4167 7.50744L9.67376 8.03444L9.97544 8.94095C10.5325 10.615 10.8111 11.452 10.4033 11.754C9.99553 12.056 9.27604 11.5457 7.83705 10.5249L7 9.93112L6.16295 10.5249C4.72396 11.5457 4.00447 12.056 3.5967 11.754C3.18893 11.452 3.46747 10.615 4.02456 8.94095L4.04557 8.87783C4.18081 8.47145 4.24843 8.26825 4.18684 8.08006C4.12525 7.89187 3.94958 7.76725 3.59824 7.51802C2.11566 6.46633 1.37437 5.94049 1.52971 5.45302C1.68504 4.96556 2.5878 4.96556 4.39332 4.96556H5.34752L5.67163 3.99166Z" fill="{color}"></path></svg>
+          <svg width="14" height="14" viewBox="0 0 14 14" fill="none" role="img">
+            <path d="M5.67163 3.99166C6.22068 2.34179 6.49521 1.51686 7 1.51686C7.50479 1.51686 7.77932 2.34179 8.32837 3.99166L8.65248 4.96556H9.60668C11.4122 4.96556 12.315 4.96556 12.4703 5.45302C12.6256 5.94049 11.8893 6.4628 10.4167 7.50744L9.67376 8.03444L9.97544 8.94095C10.5325 10.615 10.8111 11.452 10.4033 11.754C9.99553 12.056 9.27604 11.5457 7.83705 10.5249L7 9.93112L6.16295 10.5249C4.72396 11.5457 4.00447 12.056 3.5967 11.754C3.18893 11.452 3.46747 10.615 4.02456 8.94095L4.04557 8.87783C4.18081 8.47145 4.24843 8.26825 4.18684 8.08006C4.12525 7.89187 3.94958 7.76725 3.59824 7.51802C2.11566 6.46633 1.37437 5.94049 1.52971 5.45302C1.68504 4.96556 2.5878 4.96556 4.39332 4.96556H5.34752L5.67163 3.99166Z" fill="{color}"></path>
+          </svg>
         </div>
         <div class="sc-dlfnOL fpLIrj sc-kYpsJR bzekZl">{dish['rating']}</div>
         <div class="sc-dlfnOL pDfZF sc-kYpsJR lhLgBz">{dish['rating_count']}</div>
       </div>
-      <div class="sc-fUfliA iGdIQY">
-        <div aria-hidden="true" class="sc-dlfnOL ipuZJn sc-jSUnEA jaouuY">{dish['desc']}</div>
-        <div class="sc-dlfnOL iwUTQf">more</div>
-      </div>
+      {desc_html}
     </div>
     <div aria-hidden="true" class="sc-jmAvkx PgoaT">
       <button aria-label="See more information" class="sc-iUeFge fxIkFp" style="background: rgb(246, 230, 233);">
@@ -141,29 +246,42 @@ for r in restaurants:
     html = html.replace('4.4 (4.5K+ ratings)', r['rating'])
     html = html.replace('₹350 for two', r['cost'])
     
-    # replace the cuisines. In template it is:
-    # <div class="sc-dlfnOL cgFCGx sc-bOSqUZ crkxAI">Pizzas</div>
+    # replace the cuisines.
     new_cuisines = '&nbsp;'.join([f'<a href="#"><div class="sc-dlfnOL cgFCGx sc-bOSqUZ crkxAI">{c.strip()}</div></a>' for c in r['cuisines'].split(',')])
     html = re.sub(r'<a href="search\.html\?q=pizzas"><div class="sc-dlfnOL cgFCGx sc-bOSqUZ crkxAI">Pizzas</div></a>', new_cuisines, html)
     
     # replace cover image
     html = re.sub(r'https://media-assets\.swiggy\.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_1600,h_640,c_fill/RX_THUMBNAIL/IMAGES/VENDOR/2026/6/15/4fc2475d-7187-41ee-b4b5-40c41fba3a94_448001\.JPG', r['cover'], html)
     
-    # Replace the dishes list block for Hotel Chinnus
+    # Target dishes container
+    target_dishes = None
     if r['id'] == 2:
-        # We find the Recommended block and replace all dishes inside it
-        # The block is between <div class=""><div>  and  </div><div class="_207Gy"></div></div>
+        target_dishes = chinnus_dishes
+    elif r['id'] == 3:
+        target_dishes = zam_zam_dishes
+
+    if target_dishes:
+        dishes_html = '\n'.join([render_dish(d, idx) for idx, d in enumerate(target_dishes)])
         
-        dishes_html = '</div><div class="_207Gy"></div><div></div></div><div>'.join([render_dish(d, idx) for idx, d in enumerate(chinnus_dishes)])
+        # Replace recItemsBlock content
+        marker1 = '<div class="category-dish-container" id="recItemsBlock">'
+        marker2 = '<!-- Exact Swiggy Menu FAB'
         
-        # We need a robust regex to replace the dishes inside the Recommended accordion
-        pattern = r'(<h2 class="sc-dlfnOL dVkLGg">Recommended \(\d+\)</h2>.*?</button><div class="">).*?(</div><div class="_207Gy"></div></div></div></div>)'
-        
-        replacement = r'\g<1>' + f'<div>{dishes_html}</div>' + r'\g<2>'
-        html = re.sub(pattern, replacement, html, flags=re.DOTALL)
+        if marker1 in html and marker2 in html:
+            idx1 = html.find(marker1) + len(marker1)
+            idx2 = html.find(marker2)
+            
+            # Find the closing tags right before marker2
+            prefix = html[:idx1]
+            suffix = html[idx2:]
+            
+            html = prefix + '\n' + dishes_html + '\n</div></div></div></div>\n' + suffix
+            print(f"Successfully injected dishes for restaurant {r['id']} ({r['name']})")
+        else:
+            print(f"Could not find markers for restaurant {r['id']}")
 
     filename = f'restaurant{r["id"]}.html'
     with open(filename, 'w', encoding='utf-8') as out:
         out.write(html)
         
-print("Generated 15 restaurant HTML files.")
+print("Regenerated restaurant HTML files successfully.")
